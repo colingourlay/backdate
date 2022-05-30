3a34b1283754e3e24e00415f24115b3cfb8910d3
Colin Gourlay <colin@colin-gourlay.com>
2022-05-30 18:20:30 +1000

    Add (commented-out) query for only season-related content

--- src/routes/content/[days].ts
+++ src/routes/content/[days].ts
@@ -17,6 +17,9 @@ export async function get({ params }) {
 		const dataURL = `${AIRTABLE_BASE_URL}YouTube%20Videos?fields%5B%5D=id&fields%5B%5D=title&fields%5B%5D=date${
 			offset ? `&offset=${offset}` : ''
 		}&filterByFormula=AND(days_since_epoch+%3C%3D+${daysSinceEpoch}%2C+hide+%3D+False())`;
+		// const dataURL = `${AIRTABLE_BASE_URL}YouTube%20Videos?fields%5B%5D=id&fields%5B%5D=title&fields%5B%5D=date${
+		// 	offset ? `&offset=${offset}` : ''
+		// }&filterByFormula=AND(AND(days_since_epoch+%3C%3D+${daysSinceEpoch}%2C+hide+%3D+False())%2C+has_season_content+%3D+True())`;
 
 		const dataResponse = await fetch(dataURL, {
 			method: 'GET',

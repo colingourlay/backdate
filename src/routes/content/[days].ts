6f6ca90eec1301881f4bdacd05a63f43f2c33d15
Colin Gourlay <colin@colin-gourlay.com>
2022-05-30 16:11:39 +1000

    Update airtable fetching to do round-trips for multiple pages.

--- src/routes/content/[days].ts
+++ src/routes/content/[days].ts
@@ -9,19 +9,43 @@ export async function get({ params }) {
 	const { days: backdateDays } = params;
 	const daysSinceEpoch = getCurrentDaysSincePlaydateEpoch() - backdateDays;
 
-	const dataURL = `${AIRTABLE_BASE_URL}YouTube%20Videos?filterByFormula=AND(days_since_epoch+%3C%3D+${daysSinceEpoch}%2C+hide+%3D+False())`;
+	let records = [];
+	let offset = '';
+	let isDone = false;
 
-	const dataResponse = await fetch(dataURL, {
-		method: 'GET',
-		headers: {
-			Authorization: `Bearer ${AIRTABLE_TOKEN}`,
-			'Content-Type': 'application/json'
+	while (!isDone) {
+		const dataURL = `${AIRTABLE_BASE_URL}YouTube%20Videos?fields%5B%5D=id&fields%5B%5D=title&fields%5B%5D=date${
+			offset ? `&offset=${offset}` : ''
+		}&filterByFormula=AND(days_since_epoch+%3C%3D+${daysSinceEpoch}%2C+hide+%3D+False())`;
+
+		const dataResponse = await fetch(dataURL, {
+			method: 'GET',
+			headers: {
+				Authorization: `Bearer ${AIRTABLE_TOKEN}`,
+				'Content-Type': 'application/json'
+			}
+		});
+
+		if (dataResponse.ok) {
+			const data = await dataResponse.json();
+
+			records = [...records, ...data.records];
+
+			if (data.offset) {
+				offset = data.offset;
+			} else {
+				isDone = true;
+			}
+		} else {
+			isDone = true;
 		}
-	});
+	}
 
-	if (dataResponse.ok) {
+	if (records.length > 0) {
 		return {
-			body: await dataResponse.json()
+			body: {
+				records
+			}
 		};
 	}
 

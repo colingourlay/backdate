b03927af08ca924e2fe45cba5bdbd27d4838b97a
Colin Gourlay <colin@colin-gourlay.com>
2022-05-31 12:21:34 +1000

    Add inverted focus state for Button

--- src/lib/components/Button/Button.svelte
+++ src/lib/components/Button/Button.svelte
@@ -10,11 +10,19 @@
 		display: block;
 		width: 100%;
 		padding: 0.5em 1em;
-		border: 4px solid currentColor;
+		border: 4px solid var(--screen-black-color);
 		outline-offset: 0.1875em;
 		background-color: var(--screen-white-color);
 		color: var(--screen-black-color);
 		font-weight: bold;
 		font-size: 1.25em;
+		letter-spacing: 0.02em;
+	}
+
+	button:focus,
+	button:hover,
+	button:active {
+		background-color: var(--screen-black-color);
+		color: var(--screen-white-color);
 	}
 </style>
